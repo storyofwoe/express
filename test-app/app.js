d0d1cad2311b8cac882b7df5e4cea078d176528d
juliet <53350096+storyofwoe@users.noreply.github.com>
2026-07-27 15:20:02 +0000

    init student api

--- test-app/app.js
+++ test-app/app.js
@@ -4,7 +4,40 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let students = [
+  { id: 1, name: "Juliet", course: "Mathematics" },
+  { id: 2, name: "Ruby Rose", course: "Economics" },
+  { id: 3, name: "Yang Xiao Long", course: "Philosophy" },
+  { id: 4, name: "Weiss Schnee", course: "English Literature" },
+  { id: 5, name: "Blake Belladonna", course: "Film Studies" },
+  { id: 6, name: "Pyrra Nikos", course: "Mathematics" }
+];
+
+app.get('/students', (req, res) => {
+  if (req.query.course) {
+    filteredStudents = students.filter(person => person.course === req.query.course);
+
+    res.status(200);
+    res.json(filteredStudents);
+
+  } else {
+    res.status(200);
+    res.json(students);
+  }
+});
+
+app.get('/students/:id', (req, res) => {
+  const personId = parseInt(req.params.id);
+  student = students.find(person => person.id === personId);
+  
+  res.status(200)
+  res.json(student)
+});
+
+//////////////////
+
 app.get('/', (req, res) => {
+  res.status(200)
   res.send('Hello, world!');
 });
 
