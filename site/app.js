7cd8eccaff2de17feca7a41babfd006e62ddbe20
juliet <53350096+storyofwoe@users.noreply.github.com>
2026-07-30 10:56:04 +0000

    added healthcheck

--- site/app.js
+++ site/app.js
@@ -38,6 +38,10 @@ const hashFunc = (input) => {
 //ENDPOINTS
 //-----------------
 
+app.get('/', (req, res) => {
+  return res.status(200).json({ health: 'ok' })
+})
+
 app.use((req, res, next) => {
   console.log(`${req.method}, ${req.url}`)
   next();
